0163ecd2b82ad2fdf4c64b8b6b6f8d49402ad3ff
Firathubgit <Firat05_@hotmail.com>
2025-03-15 06:38:35 +0100

    case issues

--- portfoliowebsite2/portfoliowebsite2tail/src/contactPage.jsx
+++ portfoliowebsite2/portfoliowebsite2tail/src/contactPage.jsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import Navbar from './Components/Header/Navbar.jsx'
 import Footer from './Components/Footer/Footer.jsx'
-import Contact from './Components/ContactMe/ContactMe.jsx'
+import Contact from './Components/ContactMe/contactMe.jsx'
 
 createRoot(document.getElementById("root")).render(
     <StrictMode>
